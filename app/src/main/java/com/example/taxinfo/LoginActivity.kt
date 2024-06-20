751b90157604aa833dc86df51338731d49531513
Ujjwal Malhotra <ujjwalmalhotra612@gmail.com>
2024-06-19 23:16:03 -0300

    added authentication in login and register activity

---
+++ app/src/main/java/com/example/taxinfo/LoginActivity.kt
@@ -0,0 +1,45 @@
+package com.example.taxinfo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.taxinfo.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.login.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful){
+                            startActivity(Intent(this, MainActivity::class.java))
+                        } else Toast.makeText(this, it.exception?.localizedMessage.toString()
+                            , Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "All Fields Required!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/taxinfo/RegisterActivity.kt
+++ app/src/main/java/com/example/taxinfo/RegisterActivity.kt
@@ -2,15 +2,18 @@ package com.example.taxinfo
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taxinfo.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +21,30 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.alreadyLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.register.setOnClickListener {
+            val name = binding.name.text.toString()
+            val email = binding.email.text.toString()
+            val phone = binding.phone.text.toString()
+            val password = binding.password.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty()
+                && phone.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) startActivity(Intent(this, LoginActivity::class.java))
+                        else Toast.makeText(this, it.exception?.localizedMessage.toString()
+                            ,Toast.LENGTH_SHORT).show()
+                } 
+            } else {
+                Toast.makeText(this, "All Fields Required!!", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
